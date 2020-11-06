30f93303f3a026ecbd49a7392dc9421b1474675e
yamakawa <yamakawa@yamakawa-Mac-Pro.local>
2020-11-06 11:16:18 +0900

    2020/11/05

--- header-business.php
+++ header-business.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>空プロ 青木鉄鋼</title>
+<title></title>
+<?php wp_head(); ?>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
-<meta name="description" content="">
 <meta name="keywords" content="">
 <meta name="robots" content="noindex">
 <link rel="shortcut icon" href="favicon.ico">
